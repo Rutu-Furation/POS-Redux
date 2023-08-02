eaa6dcd248a1f63f1141d37286fe81eec40a99c0
sonu kumar <sonu.kumar@furation.tech>
2023-08-02 17:31:50 +0530

    fixed2

--- src/routes/All_Routes.jsx
+++ src/routes/All_Routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   Login,
-  Settings,     
+  Settings,   
   WhiteLabel,
   AddPrinter,
   ListPrinters,
