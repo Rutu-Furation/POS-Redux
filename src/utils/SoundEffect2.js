3375bf886a980e6ab4e13cbc22183d06cbe04f7b
sachin <sachin.yadav@furation.tech>
2023-08-09 14:05:34 +0530

    sound effect updated on POS panel

--- src/utils/SoundEffect2.js
+++ src/utils/SoundEffect2.js
@@ -1,14 +1,12 @@
-const playSoundEffect = () => {
+const playSoundEffect2
+ = () => {
     const scriptURL = new URL(import.meta.url);
     const soundURL = new URL(
-      "../assets/interface-124464.mp3",
+      "../assets/stop-13692.mp3",
       scriptURL
     ).toString();
     const audio = new Audio(soundURL);
     audio.play();
   };
 
-  export default playSoundEffect
-
-
- 
+  export default playSoundEffect2
